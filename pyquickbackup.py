f286eee225b29dc91b7a439b6227876892bfdbce
Edmond Burnett <eburnett@gmail.com>
2011-11-15 01:13:08 -0800

    cleaned up code and comments a bit

--- pyquickbackup.py
+++ pyquickbackup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # PyQuickBackup
-# Simple tool for archiving & backing up files
+# Simple tool for archiving & backing up files, created primarily as an exercise
 # for Python 3.x
 
 
@@ -10,7 +10,7 @@ import re
 from sys import exit
 
 
-
+# show useless info
 def welcome_help():
     
     print("PyQuickBackup 0.1")
@@ -18,6 +18,8 @@ def welcome_help():
     print("Run with --help for command-line options.\n\n")
 
 
+
+# parse the config file, create a few lists
 def config_parse():
 
     configlist = []     # start with empty lists
@@ -25,35 +27,46 @@ def config_parse():
     excludes = []
 
     try:
-        configfile = open('pyquickbackup.conf', 'r')            # opens the config file, if it exists
+        # open config file
+        configfile = open('pyquickbackup.conf', 'r')
         
+        # remove comments, empty lines from config and append to list, stripping newline chars
         for line in configfile:
-            if re.compile('^#').search(line) is not None:       # ignore comments
+            if re.compile('^#').search(line) is not None:
                 continue
-            if not line.strip():                                # ignore empty lines
+            if not line.strip():
                 continue
             else:
-                configlist.append(line.rstrip())                # add remaining lines to configlist list
+                configlist.append(line.rstrip())
         
-        in_i = configlist.index('[includes]')                   # get index of [includes]
-        ex_i = configlist.index('[excludes]')                   # get index of [excludes]
 
+        # get index of include/exclude section designators
+        in_i = configlist.index('[includes]')                   
+        ex_i = configlist.index('[excludes]')                   
+
+        # create lists for includes and excludes
         includes = configlist[in_i:ex_i]
         excludes = configlist[ex_i:]
 
+        # remove the actual include/exclude section designators
         includes.remove('[includes]')
         excludes.remove('[excludes]')
 
 
 
-
-
-    except (IOError, UnboundLocalError):                        # if .conf file not found, throw error
+    # throw error and exit if no config file is found
+    except (IOError, UnboundLocalError):
         print("- Config file not found!")
         print("- Set up pyquickbackup.conf before running.\n")
         exit()
     
     
+# compresses and creates tar archive
+def create_archive():
+
+    pass
+
+
 
 
 
@@ -61,6 +74,7 @@ def main():
 
     welcome_help()
     config_parse()
+    create_archive()
 
 
 if __name__ == "__main__":
